2c968d9eefb51dd5ae3c6d669b2316ff7ba0d750
Somfic <somfic@gmail.com>
2018-10-22 12:04:32 +0200

    Updated testing

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
 using System.IO;
 
 using EliteAPI;
+using EliteAPI.EDDB;
+
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Example
 {
@@ -14,15 +19,19 @@ namespace Example
             EliteDangerousAPI EliteAPI = new EliteDangerousAPI(new DirectoryInfo($@"C:\Users\{Environment.UserName}\Saved Games\Frontier Developments\Elite Dangerous"));
             EliteAPI.Start();
 
-            EliteAPI.DockedEvent += EliteAPI_DockedEvent;
+            EliteAPI.EDDB.UpdateData(DataRequests.Prices);
+
+            Console.WriteLine("Searching..");
+
+            var Fusang = EliteAPI.EDDB.GetSystemByName("Fusang");
+            var CleveVision = EliteAPI.EDDB.GetStationsBySystem(Fusang).First(x => x.name == "Cleve Vision");
+
+            Console.WriteLine(JsonConvert.SerializeObject(CleveVision));
+
+            Fusang = null;
+            CleveVision = null;
 
             Thread.Sleep(-1);
-        }
-
-        private static void EliteAPI_DockedEvent(object sender, DockedInfo e)
-        {
-            //This method will be ran every time the player docks.
-            Console.WriteLine($"Docked at {e.StationName}.");
         }
     }
 } 

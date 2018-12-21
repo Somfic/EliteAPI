80ee1746b3599384f4b6ce5beafb5f29eedf46d6
Somfic <somfic@gmail.com>
2018-12-21 15:03:33 +0100

    Initial VA plugin commit

---
+++ EliteAPI/VoiceAttack/VAPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VAExtensions;
+
+namespace EliteAPI.VoiceAttack
+{
+    class VAPlugin
+    {
+        public void Start()
+        {
+            
+        }
+    }
+}

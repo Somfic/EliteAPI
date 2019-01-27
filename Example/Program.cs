55ab65005695977c2e7389763ba4237581f08477
Somfic <somfic@gmail.com>
2019-01-27 12:45:39 +0100

    Fix

--- Example/Program.cs
+++ Example/Program.cs
@@ -12,6 +12,10 @@ namespace Example
             EliteDangerousAPI api = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory);
             api.DiscordRichPresence.TurnOn();
             api.Logger.Log += (sender, e) => Console.WriteLine(e.Message);
+            api.Events.AllEvent += (sender, e) => Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(e));
+
+            api.DiscordRichPresence.TurnOn();
+
             api.Start();
 
             Thread.Sleep(-1);

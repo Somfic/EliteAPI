6dae1f78062e2e8a1fce90fce00c21682a8ed9c7
Somfic <somfic@gmail.com>
2019-01-28 18:46:15 +0100

    Fixed Example project for new release.

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 
 using EliteAPI;
 using EliteAPI.Events;
 
-using InputManager;
-
-using Microsoft.Extensions.DependencyInjection;
+//using InputManager;
 
 namespace Example
 {
     class Program
     {
+        private static EliteDangerousAPI EliteAPI;
+
         static void Main(string[] args)
         {
-            EliteAPI.VoiceAttack.EliteVA.VAPlugin.VA_Init1(null);
+            DirectoryInfo playerJournalFolder = new DirectoryInfo(
+            $@"C:\Users\{Environment.UserName}\Saved Games\Frontier Developments\Elite Dangerous");
+
+            EliteAPI = new EliteDangerousAPI(playerJournalFolder);
+
+            EliteAPI.Events.DockingGrantedEvent += EliteAPI_DockingGrantedEvent;
+
+            EliteAPI.Start();
+        }
+
+        private static void EliteAPI_DockingGrantedEvent(object sender, DockingGrantedInfo e)
+        {
+            //This method will be ran every time the player is allowed to dock.
+            if (EliteAPI.Status.Gear != true) //If the gear is not deployed, deploy it.
+            {
+                //Keyboard.KeyPress(Keys.G);
+            }
         }
     }
 }

e5163455c5335bc5bcff35ab83653147d2f3c71b
Somfic <somfic@gmail.com>
2019-03-02 19:15:07 +0100

    Started work on EDSM API.

---
+++ EliteAPI/EDSM/EDSMConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace EliteAPI.EDSM
+{
+    public class EDSMConnection
+    {
+        public string Execute(string url, List<Tuple<string, string>> Parameters)
+        {
+            //Convert the list to a HTML query.
+            List<Tuple<string, string>> parameters = new List<Tuple<string, string>>();
+            List<string> parametersLocalised = new List<string>();
+            parameters.ForEach(x => parametersLocalised.Add($"{x.Item1}={x.Item2}"));
+            string html = $"{url}?{string.Join("&", parametersLocalised)}";
+
+            Console.WriteLine(parameters.Count);
+            Console.WriteLine(html);
+
+            //Peform the GET request.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,22 +1,27 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 
 using EliteAPI;
+using EliteAPI.EDSM;
 
 namespace Example
 {
     class Program
     {
-        private static EliteDangerousAPI EliteAPI;
-
         static void Main(string[] args)
         {
-            EliteAPI = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory);
-            EliteAPI.Logger.UseConsole().UseLogFile(new DirectoryInfo(Directory.GetCurrentDirectory()));
-            //EliteAPI.Events.AllEvent += (sender, e) => EliteAPI.Logger.LogInfo(Newtonsoft.Json.JsonConvert.SerializeObject(e));
-            EliteAPI.Start();
+            //EliteAPI = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory);
+            //EliteAPI.Logger.UseConsole().UseLogFile(new DirectoryInfo(Directory.GetCurrentDirectory()));
+            ////EliteAPI.Events.AllEvent += (sender, e) => EliteAPI.Logger.LogInfo(Newtonsoft.Json.JsonConvert.SerializeObject(e));
+            //EliteAPI.Start();
 
-            Thread.Sleep(-1);
+            //Thread.Sleep(-1);
+            Console.ForegroundColor = ConsoleColor.White;
+            EDSMConnection edsm = new EDSMConnection();
+            Console.WriteLine(edsm.Execute("https://www.edsm.net/api-v1/system", new List<Tuple<string, string>>()));
+            Console.Read();
         }
     }
 }

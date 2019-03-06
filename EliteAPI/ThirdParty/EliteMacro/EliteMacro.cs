a4d9e660c0cd8e2def851b9f7b2190cf3ef53bb9
Somfic <somfic@gmail.com>
2019-03-06 18:20:50 +0100

    Rewrote both plugins to simplify readability.

--- EliteAPI/ThirdParty/EliteMacro/EliteMacro.cs
+++ EliteAPI/ThirdParty/EliteMacro/EliteMacro.cs
@@ -14,43 +14,55 @@ namespace EliteAPI.ThirdParty.EliteMacro
 {
     public class EliteMacro : vmInterface
     {
-        private static DirectoryInfo playerJournalDirectory;
-        private static EliteDangerousAPI api;
-
-        private static void SetJournalFolder()
-        {
-            if (!File.Exists("EliteMacro.ini"))
-            {
-                File.WriteAllText("EliteMacro.ini", "//Example" + Environment.NewLine);
-                File.AppendAllText("EliteMacro.ini", @"//path=D:\Saved Games\Frontier Developments");
-                playerJournalDirectory = EliteDangerousAPI.StandardDirectory;
-            }
-            else
-            {
-                try
-                {
-                    playerJournalDirectory = new DirectoryInfo(File.ReadAllLines("EliteMacro" +
-                        ".ini").Where(x => !x.StartsWith("/")).First().Split(new string[] { "path=" }, StringSplitOptions.None)[1]);
-                }
-                catch { playerJournalDirectory = EliteDangerousAPI.StandardDirectory; }
-            }
-        }
-
+        //VoiceMacro fields.
         public string DisplayName => "EliteMacro";
-
-        public string Description => "The EliteAPI plugin for VoiceMacro.";
-
+        public string Description => "EliteMacro by Somfic.";
         public string ID => new Guid("{B16F6232-5AD2-4451-BBED-C7696B41AB67}").ToString();
+
+        //EliteAPI fields.
+        private static EliteDangerousAPI EliteAPI;
+        private static ThirdPartyWrapper Wrapper;
 
         void vmInterface.Init()
         {
-            SetJournalFolder();
+            //Create new EliteAPI.
+            EliteAPI = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory);
+            EliteAPI.Logger.UseLogFile(new DirectoryInfo(vmCommand.GetDataDirectory()));
+            EliteAPI.Logger.Log += Logger_Log;
+            EliteAPI.Events.AllEvent += Events_AllEvent;
 
-            api = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory, true);
-            api.Logger.UseLogFile(new DirectoryInfo(vmCommand.GetDataDirectory()));
-            api.Logger.Log += Logger_Log;
-            api.Events.AllEvent += EliteAPI_AllEvent;
-            api.Start();
+            //Create new Wrapper.
+            Wrapper = new ThirdPartyWrapper(EliteAPI, DisplayName);
+
+            //Set the correct journal directory.
+            EliteAPI.JournalDirectory = Wrapper.GetJournalFolder("EliteMacro.ini");
+
+            //Start the API.
+            EliteAPI.Start();
+        }
+
+        void vmInterface.ReceiveParams(string Param1, string Param2, string Param3, bool Synchron)
+        {
+            Wrapper.ProcessCall(Param1);
+        }
+
+        void vmInterface.ProfileSwitched(string ProfileGUID, string ProfileName)
+        {
+        }
+
+        void vmInterface.Dispose()
+        {
+            EliteAPI.Logger.LogSuccess("Stopping VoiceMacro");
+            EliteAPI.Stop();
+        }
+
+        private static void SetVariables(List<Variable> variables)
+        {
+            foreach (Variable v in variables)
+            {
+                if(v.Type == Variable.VarType.Unknown) { continue; }
+                vmCommand.SetVariable($"EliteAPI.{v.Name}_p", v.Value.ToString());
+            }
         }
 
         private void Logger_Log(object sender, LogMessage e)
@@ -75,151 +87,23 @@ namespace EliteAPI.ThirdParty.EliteMacro
             }
         }
 
-        private void EliteAPI_AllEvent(object sender, dynamic e)
+        private void Events_AllEvent(object sender, dynamic e)
         {
-            string eventName = "";
+            string commandName = $"((EliteAPI.{Wrapper.GetEventName(e)}))";
 
-            try
+            if (commandName.Contains("Status"))
             {
-                eventName = "((EliteAPI." + e.Event + "))";
-                string macroName = vmCommand.CommandExists(eventName);
-
-                if (eventName.Contains("Status"))
-                {
-                    try
-                    {
-                        var status = api.Status;
-                        var commander = api.Commander;
-                        var location = api.Location;
-
-                        vmCommand.SetVariable("EliteAPI.Docked_p", status.Docked.ToString());
-                        vmCommand.SetVariable("EliteAPI.Landed_p", status.Landed.ToString());
-                        vmCommand.SetVariable("EliteAPI.Gear_p", status.Gear.ToString());
-                        vmCommand.SetVariable("EliteAPI.Shields_p", status.Shields.ToString());
-                        vmCommand.SetVariable("EliteAPI.Supercruise_p", status.Supercruise.ToString());
-                        vmCommand.SetVariable("EliteAPI.FlightAssist_p", status.FlightAssist.ToString());
-                        vmCommand.SetVariable("EliteAPI.MassLocked_p", status.MassLocked.ToString());
-                        vmCommand.SetVariable("EliteAPI.Hardpoints_p", status.Hardpoints.ToString());
-                        vmCommand.SetVariable("EliteAPI.Winging_p", status.Winging.ToString());
-                        vmCommand.SetVariable("EliteAPI.Lights_p", status.Lights.ToString());
-                        vmCommand.SetVariable("EliteAPI.CargoScoop_p", status.CargoScoop.ToString());
-                        vmCommand.SetVariable("EliteAPI.SilentRunning_p", status.SilentRunning.ToString());
-                        vmCommand.SetVariable("EliteAPI.Scooping_p", status.Scooping.ToString());
-                        vmCommand.SetVariable("EliteAPI.SrvHandbreak_p", status.SrvHandbreak.ToString());
-                        vmCommand.SetVariable("EliteAPI.SrvTurrent_p", status.SrvTurrent.ToString());
-                        vmCommand.SetVariable("EliteAPI.SrvNearShip_p", status.SrvNearShip.ToString());
-                        vmCommand.SetVariable("EliteAPI.SrvDriveAssist_p", status.SrvDriveAssist.ToString());
-                        vmCommand.SetVariable("EliteAPI.SrvDriveAssist_p", status.SrvDriveAssist.ToString());
-                        vmCommand.SetVariable("EliteAPI.FsdCharging_p", status.FsdCharging.ToString());
-                        vmCommand.SetVariable("EliteAPI.FsdCooldown_p", status.FsdCooldown.ToString());
-                        vmCommand.SetVariable("EliteAPI.LowFuel_p", status.LowFuel.ToString());
-                        vmCommand.SetVariable("EliteAPI.Overheating_p", status.Overheating.ToString());
-                        vmCommand.SetVariable("EliteAPI.HasLatlong_p", status.HasLatlong.ToString());
-                        vmCommand.SetVariable("EliteAPI.InDanger_p", status.InDanger.ToString());
-                        vmCommand.SetVariable("EliteAPI.InInterdiction_p", status.InInterdiction.ToString());
-                        vmCommand.SetVariable("EliteAPI.InMothership_p", status.InMothership.ToString());
-                        vmCommand.SetVariable("EliteAPI.InNoFireZone_p", status.InNoFireZone.ToString());
-                        vmCommand.SetVariable("EliteAPI.InFighter_p", status.InFighter.ToString());
-                        vmCommand.SetVariable("EliteAPI.InSRV_p", status.InSRV.ToString());
-                        vmCommand.SetVariable("EliteAPI.AnalysisMode_p", status.AnalysisMode.ToString());
-                        vmCommand.SetVariable("EliteAPI.NightVision_p", status.NightVision.ToString());
-                        vmCommand.SetVariable("EliteAPI.Pips.Systems_p", status.Pips[0].ToString());
-                        vmCommand.SetVariable("EliteAPI.Pips.Engines_p", status.Pips[1].ToString());
-                        vmCommand.SetVariable("EliteAPI.Pips.Weapons_p", status.Pips[2].ToString());
-                        vmCommand.SetVariable("EliteAPI.FireGroup_p", status.FireGroup.ToString());
-                        vmCommand.SetVariable("EliteAPI.GuiFocus_p", status.GuiFocus.ToString());
-                        vmCommand.SetVariable("EliteAPI.Fuel_p", status.Fuel.FuelMain.ToString());
-                        vmCommand.SetVariable("EliteAPI.FuelReservoir_p", status.Fuel.FuelReservoir.ToString());
-                        vmCommand.SetVariable("EliteAPI.Cargo_p", status.Cargo.ToString());
-
-                        vmCommand.SetVariable("EliteAPI.Commander_p", commander.Commander);
-                        vmCommand.SetVariable("EliteAPI.System_p", location.StarSystem);
-                        vmCommand.SetVariable("EliteAPI.Body_p", location.Body);
-                        vmCommand.SetVariable("EliteAPI.BodyType_p", location.BodyType);
-
-                        vmCommand.SetVariable("EliteAPI.Rank.Combat_p", commander.CombatRankLocalised);
-                        vmCommand.SetVariable("EliteAPI.Rank.Trade_p", commander.TradeRankLocalised);
-                        vmCommand.SetVariable("EliteAPI.Rank.Exploration_p", commander.ExplorationRankLocalised);
-
-                        vmCommand.SetVariable("EliteAPI.JumpRange_p", status.JumpRange.ToString());
-                        vmCommand.SetVariable("EliteAPI.FuelMax_p", status.Fuel.MaxFuel.ToString());
-                    }
-                    catch (Exception ex)
-                    {
-                        api.Logger.LogError($"There was an error while setting some of the status variables. (Make sure the game is running.)", ex);
-                    }
-                }
-
-                if (macroName != null)
-                {
-                    JObject attributesAsJObject = JsonConvert.DeserializeObject<JObject>(JsonConvert.SerializeObject(e));
-                    Dictionary<string, object> values = attributesAsJObject.ToObject<Dictionary<string, object>>();
-
-                    foreach (var key in values)
-                    {
-                        string type = key.Value.GetType().ToString().Replace("System.", "").Replace("Collections.Generic.", "").ToLower();
-                        string name = key.Key;
-                        string value = key.Value.ToString();
-
-                        try
-                        {
-                            if (type.Contains("int")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("long")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("string")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("decimal")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("double")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("float")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                            else if (type.Contains("bool")) { vmCommand.SetVariable("EliteAPI.Event." + name + "_p", value); }
-                        }
-                        catch (Exception ex)
-                        {
-                            api.Logger.LogError($"There was an error while trying to parse field ['{name}' ({value})] for '{eventName}'.", ex);
-                        }
-                    }
-                    vmCommand.ExecuteMacro(macroName);
-                }
+                //Set status variables.
+                SetVariables(Wrapper.GetVariables());
             }
-            catch (Exception ex)
+
+            string macroGUID = vmCommand.CommandExists(commandName);
+            if (macroGUID != null)
             {
-                api.Logger.LogError($"There was an error while invoking the event '{eventName}'.", ex);
+                //Set event variables.
+                SetVariables(Wrapper.GetEventVariables(e));
+                vmCommand.ExecuteMacro(macroGUID);
             }
-        }
-
-        void vmInterface.Dispose()
-        {
-            api.Logger.LogSuccess("Stopping EliteMacro.");
-            api.Stop();
-        }
-
-        void vmInterface.ProfileSwitched(string ProfileGUID, string ProfileName)
-        {
-            //throw new NotImplementedException();
-        }
-
-        void vmInterface.ReceiveParams(string Param1, string Param2, string Param3, bool Synchron)
-        {
-            try
-            {
-                string command = Param1.ToLower();
-
-                if (command == "updatejournal")
-                {
-                    SetJournalFolder();
-                    api = new EliteDangerousAPI(playerJournalDirectory);
-                    return;
-                }
-                else if (command == "drp on")
-                {
-                    api.DiscordRichPresence.TurnOn();
-                    return;
-                }
-                else if (command == "drp off")
-                {
-                    api.DiscordRichPresence.TurnOff();
-                    return;
-                }
-            }
-            catch { }
         }
     }
 }
--- EliteAPI/ThirdParty/EliteVA/EliteVA.cs
+++ EliteAPI/ThirdParty/EliteVA/EliteVA.cs
@@ -1,58 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace EliteAPI.ThirdParty.EliteVA
 {
     public class VAPlugin
     {
-        private static DirectoryInfo playerJournalDirectory;
-        private static EliteDangerousAPI api;
-        private static dynamic _vaProxy;
+        //VoiceAttack fields.
+        public static string VA_DisplayName() => "EliteVA";
+        public static string VA_DisplayInfo() => "EliteVA by Somfic.";
+        public static Guid VA_Id() => new Guid("{B16F6232-5AD2-4451-BBED-C7696B41AB67}");
 
-        private static void SetJournalFolder()
-        {
-            if (!File.Exists("EliteVA.ini"))
-            {
-                File.WriteAllText("EliteVA.ini", "//Example" + Environment.NewLine);
-                File.AppendAllText("EliteVA.ini", @"//path=D:\Saved Games\Frontier Developments");
-                playerJournalDirectory = EliteDangerousAPI.StandardDirectory;
-            }
-            else
-            {
-                try
-                {
-                    playerJournalDirectory = new DirectoryInfo(File.ReadAllLines("EliteVA.ini").Where(x => !x.StartsWith("/")).First().Split(new string[] { "path=" }, StringSplitOptions.None)[1]);
-                }
-                catch { playerJournalDirectory = EliteDangerousAPI.StandardDirectory; }
-            }
-        }
+        //Private VoiceAttack fields.
+        private static dynamic proxy;
 
-        public static string VA_DisplayName() { return "EliteVA"; }
-
-        public static string VA_DisplayInfo() { return "EliteVA by Somfic"; }
-
-        public static Guid VA_Id() { return new Guid("{B16F6232-5AD2-4451-BBED-C7696B41AB67}"); }
+        //EliteAPI fields.
+        private static EliteDangerousAPI EliteAPI;
+        private static ThirdPartyWrapper Wrapper;
 
         public static void VA_Init1(dynamic vaProxy)
         {
-            _vaProxy = vaProxy;
+            proxy = vaProxy;
 
-            SetJournalFolder();
+            //Create new EliteAPI.
+            EliteAPI = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory);
+            EliteAPI.Logger.UseLogFile(new DirectoryInfo(Directory.GetCurrentDirectory()));
+            EliteAPI.Logger.Log += Logger_Log;
+            EliteAPI.Events.AllEvent += Events_AllEvent;
 
-            api = new EliteDangerousAPI(playerJournalDirectory, true);
-            api.Logger.UseLogFile(new DirectoryInfo(Directory.GetCurrentDirectory()));
-            api.Logger.Log += Logger_Log;
-            api.Events.AllEvent += EliteAPI_AllEvent;
-            api.Start();
+            //Create new Wrapper.
+            Wrapper = new ThirdPartyWrapper(EliteAPI, VA_DisplayName());
 
-            _vaProxy.SetText("EliteAPI.Version", api.Version);
+            //Set the correct journal directory.
+            EliteAPI.JournalDirectory = Wrapper.GetJournalFolder("EliteVA.ini");
 
-            VA_Invoke1(vaProxy);
+            //Start the API.
+            EliteAPI.Start();
+        }
+
+        public static void VA_Invoke1(dynamic vaProxy)
+        {
+            proxy = vaProxy;
+            Wrapper.ProcessCall(proxy.Context.ToString());
+        }
+
+        public static void VA_StopCommand()
+        {
+            
+        }
+
+        public static void VA_Exit1(dynamic vaProxy)
+        {
+            proxy = vaProxy;
+            EliteAPI.Logger.LogSuccess("Stopping EliteVA.");
+            EliteAPI.Stop();
+        }
+
+        private static void SetVariables(List<Variable> variables)
+        {
+            foreach (Variable v in variables)
+            {
+                switch(v.Type)
+                {
+                    case Variable.VarType.Bool:
+                        proxy.SetBoolean("EliteAPI." + v.Name, bool.Parse(v.Value.ToString()));
+                        break;
+
+                    case Variable.VarType.Decimal:
+                        proxy.SetDecimal("EliteAPI." + v.Name, decimal.Parse(v.Value.ToString()));
+                        break;
+
+                    case Variable.VarType.String:
+                        proxy.SetText("EliteAPI." + v.Name, v.Value.ToString());
+                        break;
+
+                    case Variable.VarType.Int:
+                        proxy.SetInt("EliteAPI." + v.Name, int.Parse(v.Value.ToString()));
+                        break;
+                }
+            }
         }
 
         private static void Logger_Log(object sender, Logging.LogMessage e)
@@ -60,175 +86,39 @@ namespace EliteAPI.ThirdParty.EliteVA
             switch (e.Severity)
             {
                 case Logging.Severity.Error:
-                    _vaProxy.WriteToLog("EliteVA - " + e.Message, "red");
+                    proxy.WriteToLog("EliteVA - " + e.Message, "red");
                     break;
 
                 case Logging.Severity.Warning:
-                    _vaProxy.WriteToLog("EliteVA - " + e.Message, "orange");
+                    proxy.WriteToLog("EliteVA - " + e.Message, "orange");
                     break;
 
                 case Logging.Severity.Success:
-                    _vaProxy.WriteToLog("EliteVA - " + e.Message, "green");
+                    proxy.WriteToLog("EliteVA - " + e.Message, "green");
                     break;
 
                 case Logging.Severity.Info:
-                    _vaProxy.WriteToLog("EliteVA - " + e.Message, "blue");
+                    proxy.WriteToLog("EliteVA - " + e.Message, "blue");
                     break;
             }
         }
 
-        private static void EliteAPI_AllEvent(object sender, dynamic e)
+        private static void Events_AllEvent(object sender, dynamic e)
         {
-            string eventName = "";
+            string commandName = $"((EliteAPI.{Wrapper.GetEventName(e)}))";
 
-            try
+            if (commandName.Contains("Status"))
             {
-                eventName = "((EliteAPI." + e.@Event + "))";
-
-                if (eventName.Contains("Status"))
-                {
-
-                    try
-                    {
-                        var status = api.Status;
-                        var commander = api.Commander;
-                        var location = api.Location;
-
-                        _vaProxy.SetBoolean("EliteAPI.DOCKED", status.Docked);
-                        _vaProxy.SetBoolean("EliteAPI.LANDED", status.Landed);
-                        _vaProxy.SetBoolean("EliteAPI.GEAR", status.Gear);
-                        _vaProxy.SetBoolean("EliteAPI.SHIELDS", status.Shields);
-                        _vaProxy.SetBoolean("EliteAPI.SUPERCRUISE", status.Supercruise);
-                        _vaProxy.SetBoolean("EliteAPI.FLIGHTASSIST", status.FlightAssist);
-                        _vaProxy.SetBoolean("EliteAPI.HARDPOINTS", status.Hardpoints);
-                        _vaProxy.SetBoolean("EliteAPI.WINGING", status.Winging);
-                        _vaProxy.SetBoolean("EliteAPI.LIGHTS", status.Lights);
-                        _vaProxy.SetBoolean("EliteAPI.CARGOSCOOP", status.CargoScoop);
-                        _vaProxy.SetBoolean("EliteAPI.SILENTRUNNING", status.SilentRunning);
-                        _vaProxy.SetBoolean("EliteAPI.SCOOPING", status.Scooping);
-                        _vaProxy.SetBoolean("EliteAPI.SRVHANDBREAK", status.SrvHandbreak);
-                        _vaProxy.SetBoolean("EliteAPI.SRVTURRENT", status.SrvTurrent);
-                        _vaProxy.SetBoolean("EliteAPI.SRVNEARSHIP", status.SrvNearShip);
-                        _vaProxy.SetBoolean("EliteAPI.SRVDRIVEASSIST", status.SrvDriveAssist);
-                        _vaProxy.SetBoolean("EliteAPI.MASSLOCKED", status.MassLocked);
-                        _vaProxy.SetBoolean("EliteAPI.FSDCHARGING", status.FsdCharging);
-                        _vaProxy.SetBoolean("EliteAPI.FSDCOOLDOWN", status.FsdCooldown);
-                        _vaProxy.SetBoolean("EliteAPI.LOWFUEL", status.LowFuel);
-                        _vaProxy.SetBoolean("EliteAPI.OVERHEATING", status.Overheating);
-                        _vaProxy.SetBoolean("EliteAPI.HASLATLONG", status.HasLatlong);
-                        _vaProxy.SetBoolean("EliteAPI.INDANGER", status.InDanger);
-                        _vaProxy.SetBoolean("EliteAPI.ININTERDICTION", status.InInterdiction);
-                        _vaProxy.SetBoolean("EliteAPI.INMOTHERSHIP", status.InMothership);
-                        _vaProxy.SetBoolean("EliteAPI.INNOFIREZONE", status.InNoFireZone);
-                        _vaProxy.SetBoolean("EliteAPI.INFIGHTER", status.InFighter);
-                        _vaProxy.SetBoolean("EliteAPI.INSRV", status.InSRV);
-                        _vaProxy.SetBoolean("EliteAPI.ANALYSISMODE", status.AnalysisMode);
-                        _vaProxy.SetBoolean("EliteAPI.NIGHTVISION", status.NightVision);
-                        _vaProxy.SetInt("EliteAPI.Pips.SYSTEMS", (int)status.Pips[0]);
-                        _vaProxy.SetInt("EliteAPI.Pips.ENGINES", (int)status.Pips[1]);
-                        _vaProxy.SetInt("EliteAPI.Pips.WEAPONS", (int)status.Pips[2]);
-                        _vaProxy.SetInt("EliteAPI.FIREGROUP", (int)status.FireGroup);
-                        _vaProxy.SetInt("EliteAPI.GUIFOCUS", (int)status.GuiFocus);
-                        _vaProxy.SetDecimal("EliteAPI.FUEL", (decimal)status.Fuel.FuelMain);
-                        _vaProxy.SetDecimal("EliteAPI.FUELRESERVOIR", (decimal)status.Fuel.FuelReservoir);
-                        _vaProxy.SetInt("EliteAPI.CARGO", (int)status.Cargo);
-
-                        _vaProxy.SetText("EliteAPI.Commander", commander.Commander);
-                        _vaProxy.SetText("EliteAPI.System", location.StarSystem);
-                        _vaProxy.SetText("EliteAPI.Body", location.Body);
-                        _vaProxy.SetText("EliteAPI.BodyType", location.BodyType);
-
-                        _vaProxy.SetInt("EliteAPI.Rank.Combat", (int)commander.CombatRank);
-                        _vaProxy.SetInt("EliteAPI.Rank.Cqc", (int)commander.CqcRank);
-                        _vaProxy.SetInt("EliteAPI.Rank.Trade", (int)commander.TradeRank);
-                        _vaProxy.SetInt("EliteAPI.Rank.Exploration", (int)commander.ExplorationRank);
-
-                        _vaProxy.SetText("EliteAPI.Rank.Combat", commander.CombatRankLocalised);
-                        _vaProxy.SetText("EliteAPI.Rank.Trade", commander.TradeRankLocalised);
-                        _vaProxy.SetText("EliteAPI.Rank.Exploration", commander.ExplorationRankLocalised);
-
-                        _vaProxy.SetDecimal("EliteAPI.JumpRange", (decimal)status.JumpRange);
-                        _vaProxy.SetDecimal("EliteAPI.FuelMax", (decimal)status.Fuel.MaxFuel);
-                    }
-                    catch (Exception ex)
-                    {
-                        api.Logger.LogError($"There was an error while setting some of the status variables. (Make sure the game is running.)", ex);
-                    }
-                }
-
-                if (_vaProxy.CommandExists(eventName))
-                {
-                    JObject attributesAsJObject = JsonConvert.DeserializeObject<JObject>(JsonConvert.SerializeObject(e));
-                    Dictionary<string, object> values = attributesAsJObject.ToObject<Dictionary<string, object>>();
-
-                    foreach (var key in values)
-                    {
-                        string type = key.Value.GetType().ToString().Replace("System.", "").Replace("Collections.Generic.", "").ToLower();
-                        string name = key.Key;
-                        string value = key.Value.ToString();
-
-                        try
-                        {
-                            if (type.Contains("int")) { _vaProxy.SetInt("EliteAPI.Event." + name, int.Parse(value)); }
-                            else if (type.Contains("long")) { _vaProxy.SetInt("EliteAPI.Event." + name, int.Parse(value)); }
-                            else if (type.Contains("string")) { _vaProxy.SetText("EliteAPI.Event." + name, value); }
-                            else if (type.Contains("decimal")) { _vaProxy.SetDecimal("EliteAPI.Event." + name, decimal.Parse(value)); }
-                            else if (type.Contains("double")) { _vaProxy.SetDecimal("EliteAPI.Event." + name, decimal.Parse(value)); }
-                            else if (type.Contains("float")) { _vaProxy.SetDecimal("EliteAPI.Event." + name, decimal.Parse(value)); }
-                            else if (type.Contains("bool")) { _vaProxy.SetBoolean("EliteAPI.Event." + name, bool.Parse(value)); }
-                        }
-                        catch (Exception ex)
-                        {
-                            api.Logger.LogError($"There was an error while trying to parse field ['{name}' ({value})] for '{eventName}'.", ex);
-                        }
-                    }
-                    _vaProxy.ExecuteCommand(eventName);
-                }
+                //Set status variables.
+                SetVariables(Wrapper.GetVariables());
             }
-            catch (Exception ex)
+
+            if (proxy.CommandExists(commandName))
             {
-                api.Logger.LogError($"There was an error while invoking the event '{eventName}'.", ex);
+                //Set event variables.
+                SetVariables(Wrapper.GetEventVariables(e));
+                proxy.ExecuteCommand(commandName);
             }
-        }
-
-        public static void VA_Exit1(dynamic vaProxy)
-        {
-            _vaProxy = vaProxy;
-            api.Logger.LogSuccess("Stopping EliteVA.");
-            api.Stop();
-        }
-
-        public static void VA_StopCommand()
-        {
-
-        }
-
-        public static void VA_Invoke1(dynamic vaProxy)
-        {
-            _vaProxy = vaProxy;
-
-            try
-            {
-                string command = vaProxy.Context.ToString().ToLower();
-
-                if (command == "updatejournal")
-                {
-                    SetJournalFolder();
-                    api = new EliteDangerousAPI(playerJournalDirectory);
-                    return;
-                }
-                else if (command == "drp on")
-                {
-                    api.DiscordRichPresence.TurnOn();
-                    return;
-                }
-                else if (command == "drp off")
-                {
-                    api.DiscordRichPresence.TurnOff();
-                    return;
-                }
-            }
-            catch { }
         }
     }
 }

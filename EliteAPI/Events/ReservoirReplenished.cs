b8d84efcf4aff13f88124b9811358fff15952b2d
Somfic <somfic@gmail.com>
2019-04-10 17:40:49 +0200

    Added ReservoirReplenished event.

---
+++ EliteAPI/Events/ReservoirReplenished.cs
@@ -0,0 +1,47 @@
+﻿namespace EliteAPI.Events
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class ReservoirReplenishedInfo
+    {
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("event")]
+        public string Event { get; set; }
+
+        [JsonProperty("FuelMain")]
+        public double FuelMain { get; set; }
+
+        [JsonProperty("FuelReservoir")]
+        public double FuelReservoir { get; set; }
+    }
+
+    public partial class ReservoirReplenishedInfo
+    {
+        public static ReservoirReplenishedInfo FromJson(string json) => JsonConvert.DeserializeObject<ReservoirReplenishedInfo>(json, EliteAPI.Events.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this ReservoirReplenishedInfo self) => JsonConvert.SerializeObject(self, EliteAPI.Events.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}

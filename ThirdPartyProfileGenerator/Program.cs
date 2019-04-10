691c2f1120b8a51fc343c34cbe581ce1c01bd349
Somfic <somfic@gmail.com>
2019-04-10 17:20:31 +0200

    Started work on automatic profile generation.

--- ThirdPartyProfileGenerator/Program.cs
+++ ThirdPartyProfileGenerator/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Code2Profile.VoiceMacro;
+using EliteAPI;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ThirdPartyProfileGenerator
 {
@@ -10,6 +10,34 @@ namespace ThirdPartyProfileGenerator
     {
         static void Main(string[] args)
         {
+            EliteDangerousAPI EliteAPI = new EliteDangerousAPI(EliteDangerousAPI.StandardDirectory, true);
+            EliteAPI.Logger.UseConsole();
+            EliteAPI.Start();
+
+            VoiceMacroBuilder v = new VoiceMacroBuilder();
+            v.CreateProfile("EliteAPI VMP (auto-gen)");
+
+            //Landing gear.
+            v.AddCommand(new CommandBuilder()
+                .UsePhrase("lower landing gear")
+                .AddAction(new ConditionAction()
+                {
+                    Condition = "EliteAPI.Gear_p = False",
+                    ActionsIfTrue = new List<IAction>() { new KeyboardAction() { Key = Keys.L } }
+                })
+                );
+            v.AddCommand(new CommandBuilder()
+                .UsePhrase("raise landing gear")
+                .AddAction(new ConditionAction()
+                {
+                    Condition = "EliteAPI.Gear_p = True",
+                    ActionsIfTrue = new List<IAction>() { new KeyboardAction() { Key = Keys.L } }
+                }));
+            v.AddCommand(new CommandBuilder()
+                .UsePhrase("landing gear")
+                .AddAction(new KeyboardAction() { Key = Keys.L }));
+
+            v.BuildProfile(new DirectoryInfo(Directory.GetCurrentDirectory()));
         }
     }
 }

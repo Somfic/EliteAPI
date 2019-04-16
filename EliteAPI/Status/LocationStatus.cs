51f36256245f5b92cf97776bb6862299b7ecbd86
Somfic <somfic@gmail.com>
2019-04-16 11:44:28 +0200

    Fixed bug with LocationStatus not detecting planet stations.

--- EliteAPI/Status/LocationStatus.cs
+++ EliteAPI/Status/LocationStatus.cs
@@ -6,6 +6,7 @@
         {
             api.Events.LocationEvent += (sender, e) => { StarSystem = e.StarSystem; Body = e.Body; BodyType = e.BodyType; };
             api.Events.ApproachBodyEvent += (sender, e) => { StarSystem = e.StarSystem; Body = e.Body; BodyType = "Planet"; };
+            api.Events.ApproachSettlementEvent += (sender, e) => { Station = e.Name; BodyType = "Planet"; };
             api.Events.LeaveBodyEvent += (sender, e) => { StarSystem = e.StarSystem; Body = e.Body; BodyType = "Planet"; };
             api.Events.DockedEvent += (sender, e) => { Station = e.StationName; };
             api.Events.DockingRequestedEvent += (sender, e) => { Station = e.StationName; };

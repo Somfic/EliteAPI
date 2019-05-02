4a4e1e8daecf849b571c15729d09b6008f9fe875
Somfic <somfic@gmail.com>
2019-05-02 17:31:49 +0200

    Release 2.0.2.6.

--- EliteAPI/EliteDangerousAPI.cs
+++ EliteAPI/EliteDangerousAPI.cs
@@ -52,7 +52,7 @@ namespace EliteAPI
         /// <summary>
         /// The version of EliteAPI.
         /// </summary>
-        public string Version => "2.0.2.5";
+        public string Version => "2.0.2.6";
 
         /// <summary>
         /// Whether the API is currently running.

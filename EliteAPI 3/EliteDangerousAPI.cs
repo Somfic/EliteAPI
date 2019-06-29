71e35e0fb442cafee925516bcfa76de5226f255d
Somfic <somfic@gmail.com>
2019-06-29 15:16:03 +0200

    Started working on EliteAPI 3.

---
+++ EliteAPI 3/EliteDangerousAPI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EliteAPI
+{
+    /// <summary>
+    /// The main EliteAPI class. This class contains items and services that are able to tell you more
+    /// about the current state of the game.
+    /// </summary>
+    public class EliteDangerousAPI
+    {
+        /// <summary>
+        /// The constructor for the the EliteDangerousAPI class.
+        /// </summary>
+        public EliteDangerousAPI( )
+        {
+        }
+
+        /// <summary>
+        /// Start processing Journal files and enable the events.
+        /// </summary>
+        public void Start( )
+        {
+            //Create a stopwatch object to keep track of the process.
+
+            //Get the Journal directory from the configuration file.
+
+            //Check if the Journal directory exists.
+
+            //If it does not exist, try again with the default location.
+
+            //If the default location directory does not exist, throw an error and return.
+
+            //Look for a newer version via GitHub.
+
+            //If there's a newer version available, let the user know, but continue.
+
+            //Grab the appropiate Journal file.
+
+            //Grab the Status, Cargo, Shipyard, Outfitting, Market and ModulesInfo files.
+
+            //Pre-process the Journal file to get the API up-to-date.
+
+            //Mark the API as running.
+
+            //Stop the stopwatch.
+
+            //Start an infinite Async loop that reads the Journal file and processes when needed.
+        }
+    }
+}

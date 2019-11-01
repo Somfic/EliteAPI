653dfedf172d1e41e31582e9074b912f6d7d4ea8
Somfic <somfic@gmail.com>
2019-11-01 16:04:54 +0100

    Updated NuGet packages.

--- Example/Properties/AssemblyInfo.cs
+++ Example/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.55.2088")]
-[assembly: AssemblyFileVersion("1.0.54.2088")]
+[assembly: AssemblyVersion("1.0.56.2089")]
+[assembly: AssemblyFileVersion("1.0.55.2089")]
 
-[assembly: AssemblyInformationalVersion("1.0.57")]
+[assembly: AssemblyInformationalVersion("1.0.58")]

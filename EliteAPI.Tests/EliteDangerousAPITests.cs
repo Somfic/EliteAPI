999d7c72231c02294ec21b814430048d6780879c
Somfic <somfic@gmail.com>
2020-02-03 01:12:41 +0100

    Created Test project

---
+++ EliteAPI.Tests/EliteDangerousAPITests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EliteAPI;
+
+namespace EliteAPI.Tests
+{
+    public class EliteDangerousAPITests
+    {
+         public void 
+    }
+}

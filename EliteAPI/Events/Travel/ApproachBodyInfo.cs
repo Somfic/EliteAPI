e4a79ec30f19e06c4ab1f0fa5667e15d6ae7bcea
Somfic <somfic@gmail.com>
2020-02-10 16:07:25 +0100

    Moved ApproachBody event to correct category

--- EliteAPI/Events/Travel/ApproachBodyInfo.cs
+++ EliteAPI/Events/Travel/ApproachBodyInfo.cs
@@ -1,18 +1,35 @@
 using Newtonsoft.Json;
 
-namespace EliteAPI.Events
+namespace EliteAPI.Events.Travel
 {
+    /// <summary>
+    /// This event is written when in supercruise when entering a body's Orbital Zone.
+    /// </summary>
     public class ApproachBodyInfo : EventBase
     {
+        internal ApproachBodyInfo() { }
+
+        /// <summary>
+        /// The starsystem of the body.
+        /// </summary>
         [JsonProperty("StarSystem")]
         public string StarSystem { get; internal set; }
 
+        /// <summary>
+        /// The address of the starsystem.
+        /// </summary>
         [JsonProperty("SystemAddress")]
         public long SystemAddress { get; internal set; }
 
+        /// <summary>
+        /// The name of the body that is being approached.
+        /// </summary>
         [JsonProperty("Body")]
         public string Body { get; internal set; }
 
+        /// <summary>
+        /// The ID of the body that is being approached.
+        /// </summary>
         [JsonProperty("BodyID")]
         public long BodyId { get; internal set; }
 

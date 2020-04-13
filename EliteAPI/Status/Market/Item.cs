da438c010ad260c42e306a3bc54f257fa1faed27
Somfic <somfic@gmail.com>
2020-04-13 20:55:49 +0200

    Created Market status objects

---
+++ EliteAPI/Status/Market/Item.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace EliteAPI.Status.Market
+{
+    public class Item
+    {
+        internal Item() { }
+
+        [JsonProperty("id")]
+        public long Id { get; internal set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; internal set; }
+
+        [JsonProperty("Name_Localised")]
+        public string NameLocalised { get; internal set; }
+
+        [JsonProperty("Category")]
+        public string Category { get; internal set; }
+
+        [JsonProperty("Category_Localised")]
+        public string CategoryLocalised { get; internal set; }
+
+        [JsonProperty("BuyPrice")]
+        public long BuyPrice { get; internal set; }
+
+        [JsonProperty("SellPrice")]
+        public long SellPrice { get; internal set; }
+
+        [JsonProperty("MeanPrice")]
+        public long MeanPrice { get; internal set; }
+
+        [JsonProperty("StockBracket")]
+        public long StockBracket { get; internal set; }
+
+        [JsonProperty("DemandBracket")]
+        public long DemandBracket { get; internal set; }
+
+        [JsonProperty("Stock")]
+        public long Stock { get; internal set; }
+
+        [JsonProperty("Demand")]
+        public long Demand { get; internal set; }
+
+        [JsonProperty("Consumer")]
+        public bool Consumer { get; internal set; }
+
+        [JsonProperty("Producer")]
+        public bool Producer { get; internal set; }
+
+        [JsonProperty("Rare")]
+        public bool Rare { get; internal set; }
+    }
+}
---
+++ EliteAPI/Status/Market/MarketStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EliteAPI.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Status.Market
+{
+    public class MarketStatus : EventBase
+    {
+        internal MarketStatus() { }
+
+        [JsonProperty("MarketID")]
+        public long MarketId { get; internal set; }
+
+        [JsonProperty("StationName")]
+        public string StationName { get; internal set; }
+
+        [JsonProperty("StarSystem")]
+        public string StarSystem { get; internal set; }
+
+        [JsonProperty("Items")]
+        public IReadOnlyList<Item> Items { get; internal set; }
+    }
+}

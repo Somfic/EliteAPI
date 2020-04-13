65662a7886cf5638ef27ec153ab0f0ea2cc5d7bc
Somfic <somfic@gmail.com>
2020-04-13 20:56:33 +0200

    Created Shipyard status objects

---
+++ EliteAPI/Status/Shipyard/PriceList.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace EliteAPI.Status.Shipyard
+{
+    public class PriceList
+    {
+        internal PriceList() { }
+
+        [JsonProperty("id")]
+        public long Id { get; internal set; }
+
+        [JsonProperty("ShipType")]
+        public string ShipType { get; internal set; }
+
+        [JsonProperty("ShipPrice")]
+        public long ShipPrice { get; internal set; }
+
+        [JsonProperty("ShipType_Localised", NullValueHandling = NullValueHandling.Ignore)]
+        public string ShipTypeLocalised { get; internal set; }
+    }
+}
---
+++ EliteAPI/Status/Shipyard/ShipyardStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EliteAPI.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Status.Shipyard
+{
+    public class ShipyardStatus : EventBase
+    {
+        internal ShipyardStatus() { }
+
+        [JsonProperty("MarketID")]
+        public long MarketId { get; internal set; }
+
+        [JsonProperty("StationName")]
+        public string StationName { get; internal set; }
+
+        [JsonProperty("StarSystem")]
+        public string StarSystem { get; internal set; }
+
+        [JsonProperty("Horizons")]
+        public bool Horizons { get; internal set; }
+
+        [JsonProperty("AllowCobraMkIV")]
+        public bool AllowCobraMkIv { get; internal set; }
+
+        [JsonProperty("PriceList")]
+        public IReadOnlyList<PriceList> PriceList { get; internal set; }
+    }
+}

1a4ccc32ee2bb9de6c6ea4792c87d695a4737061
Somfic <somfic@gmail.com>
2020-04-13 22:03:01 +0200

    Create StatusReader.cs

---
+++ EliteAPI/Status/StatusReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Somfic.Logging;
+using System.IO;
+using EliteAPI.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Status
+{
+    internal static class StatusReader
+    {
+        public static T Read<T>(string path) where T : IStatus
+        {
+            return JsonConvert.DeserializeObject<T>(FileReader.ReadAllText(path));
+        }
+    }
+}

84ac26158fbf07df59e0e6ebac87419792a3c182
Somfic <somfic@gmail.com>
2020-04-13 20:55:59 +0200

    Created Modules status objects

---
+++ EliteAPI/Status/Modules/ModulesStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EliteAPI.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Status.Modules
+{
+    public class ModulesStatus : EventBase
+    {
+        internal ModulesStatus() { }
+
+        [JsonProperty("Modules")]
+        public IReadOnlyList<Module> Modules { get; internal set; }
+    }
+}

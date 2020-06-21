5d9d3a6c13080e403ac22182479def8d5db2fc00
Somfic <somfic@gmail.com>
2020-06-21 21:03:46 +0200

    Update StatusEvent.cs

--- EliteAPI/Events/StatusEvent.cs
+++ EliteAPI/Events/StatusEvent.cs
@@ -7,27 +7,12 @@ namespace EliteAPI.Events
         public StatusEvent(string @event, object value)
         {
             this.Event = @event;
-            this.@event = @event;
             this.Value = value;
-            this.value = value;
 
             this.Timestamp = DateTime.Now;
         }
 
         public string Event { get; set; }
-
-        public string @event
-        {
-            get => Event;
-            set => Event = value;
-        }
-
         public dynamic Value { get; set; }
-
-        public dynamic value
-        {
-            get => Value;
-            set => Value = value;
-        }
     }
 }

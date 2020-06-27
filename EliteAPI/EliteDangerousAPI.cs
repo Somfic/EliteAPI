4c3f5763a48bad4fa95b0afe107c6483cd4160d8
Somfic <somfic@gmail.com>
2020-06-27 10:45:51 +0200

    Added Threading

--- EliteAPI/EliteDangerousAPI.cs
+++ EliteAPI/EliteDangerousAPI.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using EliteAPI.Service.Discord;
 using EliteAPI.Service.Location;

655a35e106fb9e6a81b2bc7da8b07bbe8ce1264a
Somfic <somfic@gmail.com>
2020-07-08 20:44:16 +0200

    Update JournalReader.cs

--- EliteAPI/Utilities/JournalReader.cs
+++ EliteAPI/Utilities/JournalReader.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,42 +21,79 @@ namespace EliteAPI.Utilities
             Task.Run(() =>
             {
                 FileInfo lastJournal = null;
-                List<string> processedLogs= new List<string>();
+
+                List<string> processedLogs = new List<string>();
+                Dictionary<string, string> lastEntry = EliteDangerousAPI.SupportFiles.ToDictionary(supportFile => supportFile, supportFile => "");
+
 
                 while (ShouldBeWatching)
                 {
-                    // Get the last changed journal file.
-                    FileInfo newJournal = JournalDirectory.GetActiveJournalFile(dir);
-
-                    if (lastJournal == null || lastJournal.FullName != newJournal.FullName)
+                    try
                     {
-                        // We're reading from a different file.
-                        Logger.Debug($"{newJournal.Name} selected.");
-                        lastJournal = newJournal;
 
-                        processedLogs = new List<string>();
+                        // Get the last changed journal file.
+                        FileInfo newJournal = JournalDirectory.GetActiveJournalFile(dir);
+
+                        if (lastJournal == null || lastJournal.FullName != newJournal.FullName)
+                        {
+                            // We're reading from a different file.
+                            Logger.Debug($"{newJournal.Name} selected.");
+                            lastJournal = newJournal;
+
+                            processedLogs = new List<string>();
+                        }
+
+                        if (!HasCatchedUp && catchUp)
+                        {
+                            Logger.Debug("Catching up on past events.");
+                        }
+
+                        foreach (string log in FileReader.ReadAllLines(newJournal.FullName))
+                        {
+                            if (processedLogs.Contains(log))
+                            {
+                                continue;
+                            }
+
+                            // New log entry.
+                            processedLogs.Add(log);
+                            JournalEntry?.Invoke(null, log);
+                        }
+
+                        if (!HasCatchedUp)
+                        {
+                            HasCatchedUp = true;
+
+                            if (catchUp)
+                            {
+                                Logger.Debug("Catched up on past events.");
+                            }
+                        }
+
+                        // Process the 
+                        foreach (string supportFile in EliteDangerousAPI.SupportFiles)
+                        {
+                            string path = Path.Combine(dir.FullName, supportFile);
+
+                            if (!File.Exists(path)) continue;
+
+                            string log = FileReader.ReadAllText(path);
+
+                            if (lastEntry[supportFile] == log) continue;
+
+                            // New log entry.
+                            Enum.TryParse(supportFile.Substring(0, supportFile.Length - 5), out SupportFile file);
+                            SupportEntry?.Invoke(null, (file, log));
+                            lastEntry[supportFile] = log;
+                        }
+
+                        // Check again after 250ms.
+                        Thread.Sleep(250);
                     }
-
-                    if (!HasCatchedUp && catchUp) { Logger.Debug("Catching up on past events."); }
-
-                    foreach (string log in FileReader.ReadAllLines(newJournal.FullName))
+                    catch (Exception ex)
                     {
-                        if(processedLogs.Contains(log)) continue;
-                     
-                        // New log entry.
-                        processedLogs.Add(log);
-                        LogEntry?.Invoke(null, log);
+                        Logger.Error("Could not update from log files.", ex);
                     }
-
-                    if (!HasCatchedUp)
-                    {
-                        HasCatchedUp = true;
-
-                        if (catchUp) { Logger.Debug("Catched up on past events."); }
-                    }
-
-                    // Check again after 250ms.
-                    Thread.Sleep(250);
                 }
             });
         }
@@ -65,6 +103,8 @@ namespace EliteAPI.Utilities
             ShouldBeWatching = false;
         }
 
-        internal static event EventHandler<string> LogEntry;
+        internal static event EventHandler<string> JournalEntry;
+
+        internal static event EventHandler<(SupportFile, string)> SupportEntry;
     }
 }

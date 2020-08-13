7b1bafc16c8932b9612db845670b378c7db962f9
Somfic <somfic@gmail.com>
2020-08-13 21:43:54 +0200

    Created IJournalProvider service

---
+++ EliteAPI/Journal/Provider/IJournalProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EliteAPI.Journal.Provider
+{
+    /// <summary>
+    /// Finds the journal and support files
+    /// </summary>
+    public interface IJournalProvider
+    {
+        /// <summary>
+        /// Finds the active journal file from the specified journal directory
+        /// </summary>
+        /// <param name="journalDirectory">The journal directory</param>
+        Task<FileInfo> FindJournalFile(DirectoryInfo journalDirectory);
+
+        /// <summary>
+        /// Finds the support files from the specified journal directory
+        /// </summary>
+        /// <remarks>These are the Status.json, Shipyard.json, Outfitting.json and Market.json files</remarks>
+        /// <param name="journalDirectory">The journal directory</param>
+        Task<IEnumerable<FileInfo>> FindSupportFiles(DirectoryInfo journalDirectory);
+    }
+}
---
+++ EliteAPI/Journal/Provider/JournalProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EliteAPI.Journal.Provider
+{
+    /// <inheritdoc />
+    public class JournalProvider : IJournalProvider
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<JournalProvider> _log;
+
+        public JournalProvider(IServiceProvider services)
+        {
+            _config = services.GetRequiredService<IConfiguration>();
+            _log = services.GetRequiredService<ILogger<JournalProvider>>();
+        }
+
+        /// <inheritdoc />
+        public async Task<FileInfo> FindJournalFile(DirectoryInfo journalDirectory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<FileInfo>> FindSupportFiles(DirectoryInfo journalDirectory)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

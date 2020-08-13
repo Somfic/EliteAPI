054dd38ecfbdea30da97facd1017ca90e923a638
Somfic <somfic@gmail.com>
2020-08-13 21:44:44 +0200

    Added AddEliteAPI() extension method to IServiceCollection

---
+++ EliteAPI/EliteDangerousAPIExtensions.cs
@@ -0,0 +1,27 @@
+﻿using EliteAPI.Event.Handler;
+using EliteAPI.Event.Processor;
+using EliteAPI.Event.Provider;
+using EliteAPI.Journal.Directory;
+using EliteAPI.Journal.Provider;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EliteAPI
+{
+    public static class EliteDangerousAPIExtensions
+    {
+        public static IServiceCollection AddEliteAPI<T>(this IServiceCollection services) where T : class, IEliteDangerousAPI
+        {   
+            services.AddSingleton<IEliteDangerousAPI, EliteDangerousAPI>();
+
+            services.AddSingleton<IEventProvider, EventProvider>();
+            services.AddSingleton<IEventProcessor, EventProcessor>();
+
+            services.AddSingleton<IJournalDirectoryProvider, JournalDirectoryProvider>();
+            services.AddSingleton<IJournalProvider, JournalProvider>();
+
+            //services.AddSingleton<IJournalProcessor, JournalProcessor>();
+
+            return services;
+        }
+    }
+}

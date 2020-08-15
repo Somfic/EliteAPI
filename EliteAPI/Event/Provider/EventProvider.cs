db85b1d103b3c0c1c9e8964c384d21df8cc35c0f
Somfic <somfic@gmail.com>
2020-08-15 15:52:14 +0200

    Created EventProvider implementation

--- EliteAPI/Event/Provider/EventProvider.cs
+++ EliteAPI/Event/Provider/EventProvider.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
 using EliteAPI.Event.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace EliteAPI.Event.Provider
 {
     /// <inheritdoc />
     public class EventProvider : IEventProvider
     {
-        /// <inheritdoc />
-        public EventHandler<EventBase> JournalEvent { get; }
+        private readonly ILogger<EventProvider> _log;
+
+        public EventProvider(IServiceProvider service)
+        {
+            _log = service.GetRequiredService<ILogger<EventProvider>>();
+        }
 
         /// <inheritdoc />
-        public Task ProcessJsonEvent(string json)
+        public Task<EventBase> ProcessJsonEvent(string json)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(JsonConvert.DeserializeObject<EventBase>(json));
+            }
+            catch (Exception ex)
+            {
+                ex.Data.Add("Json", json);
+
+                _log.LogTrace(ex,"Could not convert json to EventBase");
+
+                throw;
+            }
         }
     }
 }

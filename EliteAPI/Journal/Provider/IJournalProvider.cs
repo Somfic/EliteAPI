f8ff3e082e45f1aca87f44aa85b3c90a1c72d791
Somfic <somfic@gmail.com>
2020-08-15 15:52:51 +0200

    Added FindStatusFile to IJournalProvider interface

--- EliteAPI/Journal/Provider/IJournalProvider.cs
+++ EliteAPI/Journal/Provider/IJournalProvider.cs
@@ -16,9 +16,15 @@ namespace EliteAPI.Journal.Provider
         Task<FileInfo> FindJournalFile(DirectoryInfo journalDirectory);
 
         /// <summary>
+        /// Finds the status.json file from the specified journal directory
+        /// </summary>
+        /// <param name="journalDirectory">The journal directory</param>
+        Task<FileInfo> FindStatusFile(DirectoryInfo journalDirectory);
+
+        /// <summary>
         /// Finds the support files from the specified journal directory
         /// </summary>
-        /// <remarks>These are the Status.json, Shipyard.json, Outfitting.json and Market.json files</remarks>
+        /// <remarks>These are the Shipyard.json, Outfitting.json and Market.json files</remarks>
         /// <param name="journalDirectory">The journal directory</param>
         Task<IEnumerable<FileInfo>> FindSupportFiles(DirectoryInfo journalDirectory);
     }

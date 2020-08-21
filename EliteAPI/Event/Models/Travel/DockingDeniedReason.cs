7394ae49305561a582bd8224f0869ae87ddcfc8b
Somfic <somfic@gmail.com>
2020-08-21 18:58:53 +0200

    Added RestrictedAccess to DockingDenied
    With #60.

--- EliteAPI/Event/Models/Travel/DockingDeniedReason.cs
+++ EliteAPI/Event/Models/Travel/DockingDeniedReason.cs
@@ -2,6 +2,6 @@ namespace EliteAPI.Event.Models.Travel
 {
     public enum DockingDeniedReason
     {
-        NoSpace, TooLarge, Hostile, Offences, Distance, ActiveFighter, NoReason
+        NoSpace, TooLarge, Hostile, Offences, Distance, ActiveFighter, NoReason, RestrictedAccess
     }
 }

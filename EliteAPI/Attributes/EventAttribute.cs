491da02a8556d64e20bc53a0998529bed3c60a05
Somfic <somfic@gmail.com>
2020-10-02 16:22:57 +0200

    Delete EventAttribute.cs

--- EliteAPI/Attributes/EventAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EliteAPI.Attributes
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class EliteDangerousEventAttribute : Attribute
-    {
-    }
-}

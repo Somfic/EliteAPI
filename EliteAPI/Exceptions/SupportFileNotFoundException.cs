019b399a7d801c82d0fea3643b79335b26179c4e
Somfic <somfic@gmail.com>
2020-12-29 11:27:48 +0100

    Created SupportFileNotFound exception
    #115

---
+++ EliteAPI/Exceptions/SupportFileNotFoundException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using EliteAPI.Exceptions.Abstractions;
+
+namespace EliteAPI.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when a support JSON file could not be found
+    /// </summary>
+    public class SupportFileNotFoundException : EliteDangerousApiException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportFileNotFoundException"/> class
+        /// </summary>
+        public SupportFileNotFoundException()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JournalDirectoryNotFoundException"/> class with a specified error message
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        public SupportFileNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportFileNotFoundException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        /// <param name="innerException">The inner exception that caused this exception</param>
+        public SupportFileNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

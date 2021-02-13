651916f129d44429e11b320f3fc8d26f20eeb772
Lucas de Jong <lv.dejong@student.avans.nl>
2021-02-13 21:58:18 +0100

    Fixed InvitedToSquadron event naming

---
+++ EliteAPI/Event/Models/Squadrons/InvitedToSquadronEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using EliteAPI.Event.Models;
+using EliteAPI.Event.Models.Abstractions;
+
+using Newtonsoft.Json;
+
+using ProtoBuf;
+
+namespace EliteAPI.Event.Models
+{
+    
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public partial class InvitedToSquadronEvent : EventBase
+    {
+        internal InvitedToSquadronEvent() { }
+
+        [JsonProperty("SquadronName")]
+        public string Names { get; private set; }
+    }
+
+    public partial class InvitedToSquadronEvent
+    {
+        public static InvitedToSquadronEvent FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<InvitedToSquadronEvent>(json);
+        }
+    }
+}
+
+namespace EliteAPI.Event.Handler
+{
+    public partial class EventHandler
+    {
+        public event EventHandler<InvitedToSquadronEvent> InvitedToSquadronEvent;
+
+        internal void InvokeInvitedToSquadronEvent(InvitedToSquadronEvent arg)
+        {
+            InvitedToSquadronEvent?.Invoke(this, arg);
+        }
+    }
+}

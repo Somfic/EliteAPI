51e52327e56f0ce52246a19565f8627127f6daa9
Saltpile123 <ikhoudvanberichten@gmail.com>
2021-05-26 01:13:27 +0200

    Added more Odessey events: SellSuit, BuySuit, BuyWeapon, LoadoutEquipModuleEvent, UseConsumable, CollectItems

---
+++ EliteAPI/Event/Models/Other/BuyWeaponEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EliteAPI.Event.Models;
+using EliteAPI.Event.Models.Abstractions;
+
+using Newtonsoft.Json;
+
+using ProtoBuf;
+
+namespace EliteAPI.Event.Models
+{
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class BuyWeaponEvent : EventBase<BuyWeaponEvent>
+    {
+        internal BuyWeaponEvent() { }
+
+        [JsonProperty("Name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("Name_Localised")]
+        public string NameLocalised { get; private set; }
+
+        [JsonProperty("Price")]
+        public long Price { get; private set; }
+
+        [JsonProperty("SuitModuleID")]
+        public string SuitModuleId { get; private set; }
+    }
+}
+
+namespace EliteAPI.Event.Handler
+{
+    public partial class EventHandler
+    {
+        public event EventHandler<BuyWeaponEvent> BuyWeaponEvent;
+
+        internal void InvokeBuyWeaponEvent(BuyWeaponEvent arg)
+        {
+            BuyWeaponEvent?.Invoke(this, arg);
+        }
+    }
+}
---
+++ EliteAPI/Event/Models/Other/LoadoutEquipModuleEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EliteAPI.Event.Models;
+using EliteAPI.Event.Models.Abstractions;
+
+using Newtonsoft.Json;
+
+using ProtoBuf;
+
+namespace EliteAPI.Event.Models
+{
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class LoadoutEquipModuleEvent : EventBase<LoadoutEquipModuleEvent>
+    {
+        internal LoadoutEquipModuleEvent() { }
+
+        [JsonProperty("LoadoutName")]
+        public string LoadoutName { get; private set; }
+
+        [JsonProperty("SuitID")]
+        public string SuitId { get; private set; }
+
+        [JsonProperty("SuitName")]
+        public string SuitName { get; private set; }
+
+        [JsonProperty("SuitName_Localised")]
+        public string SuitNameLocalised { get; private set; }
+
+        [JsonProperty("LoadoutID")]
+        public string LoadoutId { get; private set; }
+
+        [JsonProperty("SlotName")]
+        public string SlotName { get; private set; }
+
+        [JsonProperty("ModuleName")]
+        public string ModuleName { get; private set; }
+
+        [JsonProperty("ModuleName_Localised")]
+        public string ModuleNameLocalised { get; private set; }
+
+        [JsonProperty("SuitModuleID")]
+        public string SuitModuleId { get; private set; }
+    }
+}
+
+namespace EliteAPI.Event.Handler
+{
+    public partial class EventHandler
+    {
+        public event EventHandler<LoadoutEquipModuleEvent> LoadoutEquipModuleEvent;
+
+        internal void InvokeLoadoutEquipModuleEvent(LoadoutEquipModuleEvent arg)
+        {
+            LoadoutEquipModuleEvent?.Invoke(this, arg);
+        }
+    }
+}

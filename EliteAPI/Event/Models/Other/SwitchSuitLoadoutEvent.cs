0da247f9e72c11db164e6b905c43a1d977a2e341
Saltpile123 <ikhoudvanberichten@gmail.com>
2021-05-26 12:25:00 +0200

    Added SwitchSuitLoadoutEvent and ShipLockerMaterialsEvent

---
+++ EliteAPI/Event/Models/Other/SwitchSuitLoadoutEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EliteAPI.Event.Models;
+using EliteAPI.Event.Models.Abstractions;
+
+using Newtonsoft.Json;
+
+using ProtoBuf;
+
+namespace EliteAPI.Event.Models
+{
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class SwitchSuitLoadoutEvent : EventBase<SwitchSuitLoadoutEvent>
+    {
+        internal SwitchSuitLoadoutEvent() { }
+
+        [JsonProperty("SuitID")]
+        public string SuitId { get; private set; }
+
+        [JsonProperty("SuitName")]
+        public string SuitName { get; private set; }
+
+        [JsonProperty("SuitName_Localised")]
+        public string SuitNameLocalised{ get; private set; }
+
+        [JsonProperty("LoadoutID")]
+        public string LoadoutId { get; private set; }
+
+        [JsonProperty("LoadoutName")]
+        public string LoadoutName { get; private set; }
+
+        [JsonProperty("Modules")]
+        public IReadOnlyList<SuitLoadoutEvent.LoadoutModuleInfo> Modules { get; private set; }
+    }
+}
+
+namespace EliteAPI.Event.Handler
+{
+    public partial class EventHandler
+    {
+        public event EventHandler<SwitchSuitLoadoutEvent> SwitchSuitLoadoutEvent;
+
+        internal void InvokeSwitchSuitLoadoutEvent(SwitchSuitLoadoutEvent arg)
+        {
+            SwitchSuitLoadoutEvent?.Invoke(this, arg);
+        }
+    }
+}

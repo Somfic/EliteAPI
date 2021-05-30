fc79c64e12e141da29f127989a8608bccbef2646
Lucas de Jong <somfic@gmail.com>
2021-05-31 00:36:04 +0200

    Added initial files for EliteAPI.Spansh

---
+++ EliteAPI.Spansh/SpanshExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+namespace EliteAPI.Spansh
+{
+    public static class SpanshExtensions
+    {
+        /// <summary>
+        /// Adds all Spansh's necessary services to the <seealso cref="IServiceCollection" />
+        /// </summary>
+        public static IServiceCollection AddSpansh(this IServiceCollection services)
+        {
+            services.AddTransient<IHttpClientFactory>();
+            
+            return services;
+        }
+    }
+}

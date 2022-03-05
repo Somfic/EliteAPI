02483396db1eca6d8454ba39443d495cd3fd7a74
Lucas <29230467+Somfic@users.noreply.github.com>
2022-03-05 01:42:24 +0100

    Fixed issue with null list in ship pips

--- EliteAPI.Events/Status/Ship/ShipPips.cs
+++ EliteAPI.Events/Status/Ship/ShipPips.cs
@@ -2,7 +2,7 @@
 
 public readonly struct ShipPips
 {
-    private readonly IList<int> _pips;
+    private readonly IList<int> _pips = new List<int>(3);
 
     internal ShipPips(IList<int> pips)
     {
@@ -12,15 +12,15 @@ public readonly struct ShipPips
     /// <summary>
     /// Amount of half pips set to systems
     /// </summary>
-    public int System => _pips[0];
+    public int System => _pips.ElementAtOrDefault(0);
 
     /// <summary>
     /// Amount of half pips set to engines
     /// </summary>
-    public int Engines => _pips[1];
+    public int Engines => _pips.ElementAtOrDefault(1);
 
     /// <summary>
     /// Amount of half pips set to weapons
     /// </summary>
-    public int Weapons => _pips[2];
+    public int Weapons => _pips.ElementAtOrDefault(2);
 }

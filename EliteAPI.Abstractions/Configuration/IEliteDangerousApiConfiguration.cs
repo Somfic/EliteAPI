a47b15fc6315ae8674a85a3479398416d3a78b38
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-30 01:25:42 +0100

    Allow for editing of configuration through properties

--- EliteAPI.Abstractions/Configuration/IEliteDangerousApiConfiguration.cs
+++ EliteAPI.Abstractions/Configuration/IEliteDangerousApiConfiguration.cs
@@ -4,16 +4,16 @@
 public interface IEliteDangerousApiConfiguration
 {
     /// <summary>The directory in which the journal files are located.</summary>
-    public string JournalsPath { get; }
+    public string JournalsPath { get; set; }
     
     /// <summary>The status files that will be processed.</summary>
-    public string[] StatusFiles { get; }
+    public string[]? StatusFiles { get; set; }
 
     /// <summary>The file pattern used to find journal files.</summary>
-    public string JournalPattern { get; }
+    public string JournalPattern { get; set; }
 
     /// <summary>The directory in which the option files are located.</summary>
-    public string OptionsPath { get; }
+    public string OptionsPath { get; set; }
 
     /// <summary>Applies the configuration to EliteAPI.</summary>
     void Apply();

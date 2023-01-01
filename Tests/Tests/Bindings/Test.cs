002bfb5a3905ba3662b204de15a8ccd0424ed132
Lucas <29230467+Somfic@users.noreply.github.com>
2023-01-02 00:53:47 +0100

    Fixed Secondary binding not being set correctly

--- Tests/Tests/Bindings/Test.cs
+++ Tests/Tests/Bindings/Test.cs
@@ -23,6 +23,13 @@ public class Test
     [Fact]
     public void Finding()
     {
+        var parser = new BindingsParser(Mock.Of<ILogger<BindingsParser>>());
+        // Read embedded file
+        var xml = Helper.GetResource( "Tests.Bindings.Bindings");
+
+        var parsed = parser.Parse(xml).Where(x => x.Primary?.Device == "Keyboard" || x.Secondary?.Device == "Keyboard");
         
+        parsed.Should().NotBeNull();
+        parsed.FirstOrDefault(x => x.Name == "PitchUpButton").Secondary.Should().NotBeNull();
     }
 }

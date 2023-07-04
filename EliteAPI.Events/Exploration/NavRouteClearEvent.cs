ae9b7c901255e1e2c060ead56d77211aa424ac99
Lucas <somfic@gmail.com>
2023-07-04 16:10:11 +0200

    Created NavRouteClearEvent

---
+++ EliteAPI.Events/Exploration/NavRouteClearEvent.cs
@@ -0,0 +1,13 @@
+﻿using EliteAPI.Abstractions.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Events;
+
+public readonly struct NavRouteClearEvent : IEvent
+{
+    [JsonProperty("timestamp")]
+    public DateTime Timestamp { get; init; }
+    
+    [JsonProperty("event")]
+    public string Event { get; init; }
+}

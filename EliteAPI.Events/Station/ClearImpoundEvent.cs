f3a358d4b58e66ef8cb002a5f24d5393edbaef41
Lucas <somfic@gmail.com>
2023-07-04 15:41:26 +0200

    Created ClearImpoundEvent

---
+++ EliteAPI.Events/Station/ClearImpoundEvent.cs
@@ -0,0 +1,25 @@
+﻿using EliteAPI.Abstractions.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Events;
+
+public readonly struct ClearImpoundEvent : IEvent
+{
+    [JsonProperty("timestamp")]
+    public DateTime Timestamp { get; init; }
+    
+    [JsonProperty("event")]
+    public string Event { get; init; }
+    
+    [JsonProperty("ShipType")]
+    public string ShipType { get; init; }
+    
+    [JsonProperty("ShipID")]
+    public string ShipId { get; init; }
+    
+    [JsonProperty("ShipMarketID")]
+    public string ShipMarketId { get; init; }
+    
+    [JsonProperty("MarkedID")]
+    public string MarkedId { get; init; }
+}

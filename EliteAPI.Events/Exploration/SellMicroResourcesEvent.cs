eddcec63ee30fe5a09bd4ed6d4c210fb9f4dd852
Lucas <somfic@gmail.com>
2023-07-04 16:10:29 +0200

    Created SellMicroResourcesEvent

---
+++ EliteAPI.Events/Exploration/SellMicroResourcesEvent.cs
@@ -0,0 +1,34 @@
+﻿using EliteAPI.Abstractions.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Events;
+
+public readonly struct SellMicroResourcesEvent : IEvent
+{
+    [JsonProperty("timestamp")]
+    public DateTime Timestamp { get; init; }
+    
+    [JsonProperty("event")]
+    public string Event { get; init; }
+    
+    [JsonProperty("MicroResources")]
+    public IReadOnlyCollection<MicroResourceInfo> MicroResources { get; init; }
+    
+    [JsonProperty("Price")]
+    public long Price { get; init; }
+    
+    [JsonProperty("MarketID")]
+    public string MarketId { get; init; }
+
+    public readonly struct MicroResourceInfo
+    {
+        [JsonProperty("Name")]
+        public Localised Name { get; init; }
+        
+        [JsonProperty("Category")]
+        public string Category { get; init; }
+        
+        [JsonProperty("Count")]
+        public long Count { get; init; }
+    }
+}

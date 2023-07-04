1bed0916d14a8d81a7dcaa03fb993539216b7c0b
Lucas <somfic@gmail.com>
2023-07-04 15:39:11 +0200

    Created CancelTaxiEvent

---
+++ EliteAPI.Events/Combat/CancelTaxiEvent.cs
@@ -0,0 +1,13 @@
+﻿using EliteAPI.Abstractions.Events;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Events;
+
+public readonly struct CancelTaxiEvent : IEvent
+{
+    [JsonProperty("timestamp")]
+    public DateTime Timestamp { get; init; }
+
+    [JsonProperty("event")]
+    public string Event { get; init; }
+}

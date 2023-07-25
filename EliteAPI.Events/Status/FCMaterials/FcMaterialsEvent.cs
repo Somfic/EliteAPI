4c0ee8faeaade0ee01a8972dd78f36b5d81612c4
Lucas <somfic@gmail.com>
2023-07-25 08:33:34 +0200

    Updated FCMaterials event

--- EliteAPI.Events/Status/FCMaterials/FcMaterialsEvent.cs
+++ EliteAPI.Events/Status/FCMaterials/FcMaterialsEvent.cs
@@ -1,22 +1,25 @@
 ﻿using EliteAPI.Abstractions.Events;
 using Newtonsoft.Json;
 
-namespace EliteAPI.Events;
+namespace EliteAPI.Events.Status.FCMaterials;
 
-public readonly struct FCMaterialsEvent : IEvent
+public readonly struct FcMaterialsEvent : IEvent
 {
     [JsonProperty("timestamp")]
     public DateTime Timestamp { get; init; }
-    
+
     [JsonProperty("event")]
     public string Event { get; init; }
-    
+
     [JsonProperty("MarketID")]
-    public string MarketId { get; init; }
-    
+    public long MarketId { get; init; }
+
     [JsonProperty("CarrierName")]
     public string CarrierName { get; init; }
-    
+
     [JsonProperty("CarrierID")]
     public string CarrierId { get; init; }
+
+    [JsonProperty("Items")]
+    public IReadOnlyCollection<FcMaterial> Materials { get; init; }
 }

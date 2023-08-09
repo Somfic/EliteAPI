ae5f64df653a4787515a3cb2dee6be26f1dd4a47
Lucas <29230467+Somfic@users.noreply.github.com>
2023-08-09 17:41:29 +0200

    Added EliteAPI.Web.Spansh project

---
+++ EliteAPI.Web.Spansh/SpanshApi.cs
@@ -0,0 +1,33 @@
+﻿using EliteAPI.Web.Models;
+using EliteAPI.Web.Spansh.RoutePlanner;
+using EliteAPI.Web.Spansh.Utilities;
+using Newtonsoft.Json;
+
+namespace EliteAPI.Web.Spansh;
+
+public class SpanshApi : WebApi
+{
+    public SpanshApi(IServiceProvider services) : base(services)
+    {
+        Routes = AddCategory<RoutePlannerApi>();
+        Utilities = AddCategory<UtilitiesApi>();
+    }
+
+    protected  override string BaseUrl => "https://www.spansh.co.uk/api";
+
+    public RoutePlannerApi Routes { get; }
+
+    internal UtilitiesApi Utilities { get; }
+}
+
+public interface IJobResponse : IWebApiResponse
+{
+    [JsonProperty("job")]
+    public string Job { get; init; }
+}
+
+public interface IJobRequest : IWebApiRequest
+{
+    [JsonProperty("job")]
+    public string Job { get; init; }
+}
---
+++ EliteAPI.Web.Spansh/Utilities/Requests/JobRequest.cs
@@ -0,0 +1,15 @@
+﻿using EliteAPI.Web.Models;
+
+namespace EliteAPI.Web.Spansh.Utilities.Requests;
+
+public class JobRequest : IJobRequest
+{
+    public JobRequest(string job)
+    {
+        Job = job;
+    }
+
+    public string Job { get; init; }
+    public string Endpoint => $"results/{Job}";
+    public HttpMethod Method => HttpMethod.Get;
+}

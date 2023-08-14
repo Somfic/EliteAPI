336b925bb2bb0f409c09bc1e4938164f59fd4fe4
Lucas <29230467+Somfic@users.noreply.github.com>
2023-08-14 15:04:35 +0200

    Added basic Spansh body search

---
+++ EliteAPI.Web.Spansh/Search/Requests/Bodies/Sort/DistanceSort.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace EliteAPI.Web.Spansh.Search.Requests.Sort;
+
+public class DistanceSort
+{
+    [JsonProperty("direction")]
+    public string Direction { get; init; }
+}
--- EliteAPI.Web.Spansh/SpanshApi.cs
+++ EliteAPI.Web.Spansh/SpanshApi.cs
@@ -1,5 +1,6 @@
 ﻿using EliteAPI.Web.Models;
 using EliteAPI.Web.Spansh.RoutePlanner;
+using EliteAPI.Web.Spansh.Search;
 using EliteAPI.Web.Spansh.Utilities;
 using Newtonsoft.Json;
 
@@ -10,12 +11,15 @@ public class SpanshApi : WebApi
     public SpanshApi(IServiceProvider services) : base(services)
     {
         Routes = AddCategory<RoutePlannerApi>();
+        Search = AddCategory<SearchApi>();
         Utilities = AddCategory<UtilitiesApi>();
     }
 
     protected  override string BaseUrl => "https://www.spansh.co.uk/api";
 
     public RoutePlannerApi Routes { get; }
+    
+    public SearchApi Search { get; }
 
     internal UtilitiesApi Utilities { get; }
 }

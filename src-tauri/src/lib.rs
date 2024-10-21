0fa34594851c2155bb50a59e32d498e2fd55d4a1
Lucas <lucas@somfic.dev>
2024-10-21 17:40:25 +0200

    🚚 Rename executable to `eliteapi_bin`
    semver: chore

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())

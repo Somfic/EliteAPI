afc59c4afaf2ab262d9037eaa9c281c455d3d280
Lucas <lucas@somfic.dev>
2024-10-21 17:44:39 +0200

    🚧 Add initial `rspc` setup
    semver: chore

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,31 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use rspc::Router;
+
+fn router() -> Router<()> {
+    <Router>::new()
+        .query("version", |t| t(|ctx, input: ()| env!("CARGO_PKG_VERSION")))
+        .build()
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-pub fn run() {
+pub async fn run() {
+    let router = router();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[cfg(test)]
+mod tests {
+    // Ensure the router doesn't have any issues and exports Typescript types
+    #[test]
+    fn test_rspc_router() {
+        super::router();
+    }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    eliteapi::run()
+#[tokio::main]
+async fn main() {
+    eliteapi::run().await
 }

ea1e9b2a31a104ecda77544b9da4ae314019fd27
Lucas <lucas@somfic.dev>
2025-03-11 08:40:37 +0100

    ⬆️ upgrade target framework to net8.0 in multiple projects
    semver: patch

---
+++ EliteAPI.Tests/Journals.cs
@@ -0,0 +1,70 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml;
+using EliteAPI.Abstractions.Bindings;
+using EliteAPI.Abstractions.Bindings.Models;
+using EliteAPI.Abstractions.Events;
+using EliteAPI.Bindings;
+using EliteAPI.Events;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json.Linq;
+
+namespace EliteAPI.Tests;
+
+[TestFixture]
+public class Journals
+{
+    private IEvents _events;
+    private static string[] _legacyExamples = [];
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var eventParser = new EventParser(Mock.Of<IServiceProvider>());
+        eventParser.Use<LocalisedConverter>();
+        _events = new Events.Events(Mock.Of<ILogger<Events.Events>>(), eventParser);
+        _events.Register();
+    }
+    
+    [Test(Description = "Journals")]
+    [TestCaseSource(nameof(GetJournalLines))]
+    public void Events(string json)
+    {
+        if (_legacyExamples.Any(json.Contains)) return;
+
+        // Check if the JSON is valid
+        try
+        {
+            JObject.Parse(json);
+        } catch (Exception ex)
+        {
+            ex.Data.Add("Json", json);
+            // Skip if the JSON is invalid
+            Assert.Ignore($"Invalid JSON: {ex.Message}");
+            return;
+        }
+
+        var invokedEvent = _events.Invoke(json, new EventContext());
+        
+        Assert.That(invokedEvent, Is.Not.Null, $"Event is not registered: {json}");
+        
+        // Check if the event is the correct type
+        var eventType = invokedEvent.GetType();
+        var eventName = eventType.Name;
+        if (eventName.EndsWith("Event"))
+            eventName = eventName.Substring(0, eventName.Length - 5);
+        
+        Assert.That(string.Equals(eventName, invokedEvent.Event, StringComparison.CurrentCultureIgnoreCase), $"Event is not of type {eventName} but {invokedEvent.Event}");
+    }
+    
+    static IImmutableList<string> GetJournalLines()
+    {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        return Directory
+            .GetFiles("./Journals")
+            .SelectMany(File.ReadAllLines)
+            .ToImmutableList();
+    }
+}

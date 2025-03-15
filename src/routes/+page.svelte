7721df254aee4bedd13fd8f3cab1b17ca9ec271e
Lucas <lucas@somfic.dev>
2025-03-15 17:33:22 +0100

    ⚗️ refactor event listening to use `events` module
    semver: patch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+  import { events } from "$lib/bindings";
   import SplashScreen from "$lib/screens/SplashScreen.svelte";
   import Header from "./sections/Header.svelte";
 
-  import { listen } from "@tauri-apps/api/event";
   import { onMount } from "svelte";
 
   onMount(() => {
-    listen("journal_event", (event) => {
-      console.log("journal_event", event.payload);
-    });
-
-    listen("log", (event) => {
-      console.log("log", event.payload);
+    events.variablesEvent.listen((event) => {
+      console.log("variables", event.payload.variables);
     });
   });
 </script>

52dab385b3dd0afb408348cda61ea969b2ec6355
Lucas <lucas@somfic.dev>
2025-11-22 23:23:31 +0100

    ✨ console project for testing

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using EliteApi;
+using EliteAPI.Json;
+
+var api = new EliteDangerousApi();
+api.OnJournalEvent(e =>
+{
+    var eventName = JsonUtils.GetEventName(e);
+    var paths = JsonUtils.FlattenJson(e).Select(p => p.WithPath($"EliteAPI.{eventName}.{p.Path}")).ToList();
+    foreach (var path in paths)
+    {
+        Console.WriteLine($"{path.Path}: {path.Value} ({path.Type})");
+    }
+});
+
+Thread.Sleep(-1);
